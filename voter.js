19040bb4464dabbbbf59aaf767b14cb7c1edaaf1
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-04-05 10:59:20 +0530

    valid voter

---
+++ voter.js
@@ -0,0 +1,10 @@
+//Q.Accept name and age from the user. Check if the user is a valid voter or not.
+
+let name = "Tanzeel";
+let age = 8;
+
+if(age>=18){
+    console.log(`${name} you are a valid voter`)
+}else{
+    console.log(`${name} you are not a valid voter`)
+}
