7aeff27c8b0d32818b4ad94c319f5e20be892858
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-04-05 10:46:13 +0530

    find the greatest

---
+++ greatest.js
@@ -0,0 +1,10 @@
+//Q.Accept two numbers and print the greatest between them
+
+let a = 20;
+let b = 10;
+
+if(a>b){
+    console.log(`${a} is greatest`);
+}else{
+    console.log(`${b} is greater`);
+}
